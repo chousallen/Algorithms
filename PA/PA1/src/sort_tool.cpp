8b864d0475c837290dcc2470d94acf9b9384bd7a
sallen <chouallenno@gmail.com>
2024-09-28 14:18:00 +0800

    Merge sort and bottom-up merge sort

--- PA/PA1/src/sort_tool.cpp
+++ PA/PA1/src/sort_tool.cpp
@@ -8,7 +8,8 @@
 // **************************************************************************
 
 #include "sort_tool.h"
-#include<iostream>
+#include <iostream>
+#include <string.h>
 
 void swap(int &a, int &b)
 {
@@ -111,25 +112,80 @@ void SortTool::MergeSortSubVector(vector<int>& data, int low, int high) {
     // TODO : Please complete MergeSortSubVector code here
     // Hint : recursively call itself
     //        Merge function is needed
+    if(low < high)
+    {
+        int middle = (low + high) / 2;
+        MergeSortSubVector(data, low, middle);
+        MergeSortSubVector(data, middle + 1, high);
+        Merge(data, low, middle, middle + 1, high);
+    }
 }
 
 // Merge
 void SortTool::Merge(vector<int>& data, int low, int middle1, int middle2, int high) {
     // Function : Merge two sorted subvector
     // TODO : Please complete the function
+    vector<int> tmp;
+    tmp.resize(high - low + 1);
+    int left_curr = low, right_curr = middle2, idx = 0;
+    
+    while(left_curr <= middle1 && right_curr <= high)
+    {
+        if(data[left_curr] < data[right_curr])
+        {
+            tmp[idx++] = data[left_curr++];
+        }
+        else
+        {
+            tmp[idx++] = data[right_curr++];
+        }
+    }
+    if(left_curr <= middle1)
+    {
+        memcpy(&tmp[idx], &data[left_curr], sizeof(int) * (middle1 - left_curr + 1));
+        idx += middle1 - left_curr + 1;
+    }
+    if(right_curr <= high)
+    {
+        memcpy(&tmp[idx], &data[right_curr], sizeof(int) * (high - right_curr + 1));
+        idx += high - right_curr + 1;
+    }
+    /*while(left_curr <= middle1)
+    {
+        tmp[idx++] = data[left_curr++];
+    }
+    while(right_curr <= high)
+    {
+        tmp[idx++] = data[right_curr++];
+    }*/
+    memcpy(&data[low], &tmp[0], sizeof(int) * idx);
+    /*for(int i = 0; i < idx; i++)
+    {
+        data[low + i] = tmp[i];
+    }*/
 }
 
 // bottom-up style implementation of merge sort
 void SortTool::BottomUpMergeSort(vector<int>& data) 
 {
-  /*TODO :
+    /*TODO :
     Implement merge sort in bottom-up style, in other words,
     without recursive function calls.
     Hint: 
     1. Divide data to n groups of one data each group
     2. Iteratively merge each pair of 2 neighbor groups into one larger group
     3. Finally we obtain exactly one sorted group
-  */
+    */
+    for(int width = 1; width < data.size(); width *= 2)
+    {
+        for(int i = 0; i < data.size(); i += 2 * width)
+        {
+            int low = i;
+            int middle = min(i + width - 1, (int)data.size() - 1);
+            int high = min(i + 2 * width - 1, (int)data.size() - 1);
+            Merge(data, low, middle, middle + 1, high);
+        }
+    }
 }
 
 // Heap sort method, be aware that you are NOT required to implement heap sort in PA1
@@ -138,7 +194,8 @@ void SortTool::HeapSort(vector<int>& data) {
     BuildMaxHeap(data);
     // 1. Swap data[0] which is max value and data[i] so that the max value will be in correct location
     // 2. Do max-heapify for data[0]
-    for (int i = data.size() - 1; i >= 1; i--) {
+    for (int i = data.size() - 1; i >= 1; i--)
+    {
         swap(data[0],data[i]);
         heapSize--;
         MaxHeapify(data,0);
